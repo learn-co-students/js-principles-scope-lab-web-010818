03662f7fe1ed3d93cedc3a097c3a76918fc2d1fd
TorreJohnson <torre.johnson@gmail.com>
2018-02-18 18:37:01 -0500

    completed Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+const leastFavoriteCustomer = 'Tim';
+
+function changeLeastFavoriteCustomer(name) {
+  leastFavoriteCustomer = 'Rob';
+}
