a197c5544dcb118ea59f3c1828c0d387ac5e66d3
krdiamond <github email address>
2018-02-22 14:51:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+//this accepts the customerName variable because customerName is in the global scope
+
+function setBestCustomer () {
+  bestCustomer = 'not bob';
+}
+//by not using var/let bustCustomer is set to global scope. This is bad practice
+
+function overwriteBestCustomer (newFavorite) {
+  bestCustomer = newFavorite;
+}
+//because bestCustomer is in the global scope it can easily be changed accidentally
+
+const leastFavoriteCustomer = 'sour fred';
+
+function changeLeastFavoriteCustomer  () {
+  leastFavoriteCustomer = 'not sour fred';
+}
